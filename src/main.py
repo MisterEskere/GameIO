c9746506fc26e56990f22f60f17f1fb6bbdd63cc
MisterEskere <stocazzo>
2024-06-29 20:06:02 +0200

    chore: Update .gitignore and requirements.txt, and refactor fitgirl.py and main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from fitgirl import fitgirl_search, fitgirl_get_downloadlink
+import libtorrent as lt
+from src.fitgirl import fitgirl_search, fitgirl_get_downloadlink
 
 # Specify the DNS server and the domain
 game = input("Enter the game you want to search for: ")
@@ -8,4 +9,7 @@ games = fitgirl_search(game)
 game = games[0]
 link = fitgirl_get_downloadlink(game)
 
-print(link)
+# download the magnet link with libtorrent
+ses = lt.session()
+ses.listen_on(6881, 6891)
+
